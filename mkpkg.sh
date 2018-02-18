9e2cbf9b63b2140539fa7eeb713422940aadaeda
Omer Cohen <omer@cohen.io>
2018-02-18 23:36:11 +0800

    services.txt

--- mkpkg.sh
+++ mkpkg.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+rm services.txt
 for service in `ls -d */`; do
 	service=${service%%/}
 	rm -v ${service}.zip
 	cd ${service}
 	zip -9vr ../${service}.zip *
 	cd ..
+	echo ${service} >> services.txt
 done
