fdbffb961d8f7fb494d049f5de4100102cf83137
Gal Singer <gal.singer@cymmetria.com>
2018-08-03 02:37:48 +0300

    struts service

---
+++ services/struts/struts_service.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""Struts Honeycomb Service."""
+from __future__ import unicode_literals
+
+import json
+import os
+import re
+import shutil
+import tempfile
+import time
+
+from base_service import DockerService
+
+
+class StrutsService(DockerService):
+    def __init__(self, *args, **kwargs):
+        super(StrutsService, self).__init__(*args, **kwargs)
+        self.folder_files = None
+
+    def on_server_start(self):
+        self.folder_files = tempfile.mkdtemp()
+        os.chmod(self.folder_files, 0777)
+        super(StrutsService, self).on_server_start()
+
+    def on_server_shutdown(self):
+        super(StrutsService, self).on_server_shutdown()
+        if self.folder_files and os.path.exists(self.folder_files):
+            shutil.rmtree(self.folder_files)
+
+    @property
+    def docker_params(self):
+        return dict(
+            ports={80: 80},
+            volumes={self.folder_files: {"bind": "/var/log/apache2/", "mode": "rw"}})
+
+    @property
+    def docker_image_name(self):
+        return "galcymmetria/struts_honeypot"
+
+    def parse_line(self, line):
+        result = re.match(r".*\[client .*\] (?P<json>.*)", line)
+        if not result:
+            return None
+        json_output = result.groupdict()["json"].decode("string_escape")
+        result_dict = json.loads(json_output)
+        return {
+            "event_type": "struts_exploit",
+            "originating_ip": result_dict["src"],
+            "originating_port": result_dict["sport"],
+            "request": result_dict["uri"],
+            "additional_fields": "User agent: {}, ctypes: {}".format(result_dict["ua"], result_dict["ctype"])
+        }
+
+    def get_lines(self):
+        error_log = os.path.join(self.folder_files, "error.log")
+        while True:
+            if os.path.exists(error_log):
+                break
+            time.sleep(1)
+        return self.read_lines(error_log)
+
+
+service_class = StrutsService
