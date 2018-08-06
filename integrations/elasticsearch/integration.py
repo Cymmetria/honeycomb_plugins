ad6ecd367a79f7168ab3009b8d28304f15f56346
Omer Cohen <git@omerc.net>
2018-08-06 11:45:14 -0700

    Update integration.py

--- integrations/elasticsearch/integration.py
+++ integrations/elasticsearch/integration.py
@@ -14,23 +14,15 @@ logger = logging.getLogger(__name__)
 class ElasticsearchIntegration(BaseIntegration):
 
     def send_event(self, alert_fields):
-        address = self.integration_data["url_address"]
-        port = self.integration_data["port"]
-        index = self.integration_data["index"]
         user = self.integration_data.get("username", "").encode('utf-8')
-        passwd = self.integration_data.get("password", "").encode('utf-8')
-        url = "{}:{}/{}".format(address, port, index)
-        skip_cert_validation = self.integration_data.get('skip_cert_validation', False)
-        format_alert = json.dumps(alert_fields,
-                                  indent=4,
-                                  sort_keys=True,
-                                  default=str)
-        response = requests.post(url=url,
-                                 auth=(user, passwd),
-                                 data=format_alert,
-                                 headers={"Content-type": "application/json"},
-                                 verify=not skip_cert_validation)
-        logger.info(alert_fields)
+        index = self.integration_data.get("index")
+        verify = self.integration_data.get('verify', False)
+        password = self.integration_data.get("password", "").encode('utf-8')
+
+        url = "{}/{}".format(self.integration_data.get("url"), index)
+        auth = (user, password) if (user or passwd) else None
+
+        response = requests.post(url=url, auth=auth, json=alert_fields, verify=verify)
         if response.status_code == 201:
             return {}, None
 
