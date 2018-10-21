cb874af6cc9f3e83e06c8e5daa2e30be52aa3d18
Dekel <dekel@cymmetria.local>
2018-10-21 20:02:07 +0300

    moved const

---
+++ services/cve_2018_10933/consts.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+SERVER_SIG = "SSH-2.0-libssh_0.7.4"
+
+CVE_SSH_PORT = 2200
+SSH_ALERT_TYPE = "ssh"
+CVE_ALERT_TYPE = "cve_2018_10933_ssh_auth_bypass"
+CVE_PORT_FIELD = "port"
+
+EVENT_TYPE_FIELD_NAME = "event_type"
+ORIGINATING_IP_FIELD_NAME = "originating_ip"
+ORIGINATING_PORT_FIELD_NAME = "originating_port"
+DESCRIPTION_FIELD_NAME = "event_description"
+ADDITIONAL_FIELDS_FIELD_NAME = "additional_fields"
+USERNAME_FIELD_NAME = "username"
+PASSWORD_FIELD_NAME = "password"
+KEY_FIELD_NAME = "key"
+SOCK_IP_POSITION = 0
+SOCK_PORT_POSITION = 1
