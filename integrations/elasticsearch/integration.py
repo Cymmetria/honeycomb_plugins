b1f21a0ddc07dfceca52fefb272c299e30ef6bb8
Dekel <dekelb@users.noreply.github.com>
2018-10-30 02:22:09 +0200

    fix elasticsearch json serialization issue  (#108)
    Fix the json serialization issue in the elasticsearch integration

--- integrations/elasticsearch/integration.py
+++ integrations/elasticsearch/integration.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 
 import requests
+import json
 
 from integrationmanager.exceptions import IntegrationSendEventError
 from integrationmanager.integration_utils import BaseIntegration
@@ -15,15 +16,17 @@ class ElasticsearchIntegration(BaseIntegration):
 
     def send_event(self, alert_fields):
         """Send alert to Elasticsearch."""
-        user = self.integration_data.get("username", "").encode('utf-8')
+        user = self.integration_data.get("username", "").encode("utf-8")
         index = self.integration_data.get("index")
-        verify = self.integration_data.get('verify', False)
-        password = self.integration_data.get("password", "").encode('utf-8')
+        verify = self.integration_data.get("verify", False)
+        password = self.integration_data.get("password", "").encode("utf-8")
 
         url = "{}/{}".format(self.integration_data.get("url"), index)
         auth = (user, password) if (user or password) else None
 
-        response = session.post(url=url, auth=auth, json=alert_fields, verify=verify)
+        headers = {"content-type": "application/json"}
+        data = json.dumps(alert_fields, default=str)
+        response = session.post(url=url, auth=auth, data=data, headers=headers, verify=verify)
         if response.status_code == 201:
             return {}, None
 
